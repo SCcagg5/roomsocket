820fbaaa1097b5da4c1b3e2fcac2fbc0df33f50d
SCcagg5 <sccagg5@gmail.com>
2023-04-18 10:47:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from aiohttp import web
 import socketio
 
+class User:
+    def __init__(self, sid, x, y, last_click=None):
+        self.sid = sid
+        self.x = x
+        self.y = y
+        self.last_click = last_click
+
 sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
 
 user_data = {}
+rooms = {}
 
 async def index(request):
     with open('index.html') as f:
@@ -15,34 +23,44 @@ async def index(request):
 async def join(sid, data):
     room = data['room']
     await sio.enter_room(sid, room)
-    user_data[sid] = {'x': 0, 'y': 0, 'last_click': None}
-    for user_sid, user_info in user_data.items():
+    user_data[sid] = User(sid, 0, 0)
+    if room not in rooms:
+        rooms[room] = set()
+    rooms[room].add(sid)
+    for user_sid in rooms[room]:
         if user_sid != sid:
-            await sio.emit('update_position', {'sid': user_sid, 'x': user_info['x'], 'y': user_info['y']}, room=sid)
-            if user_info['last_click']:
-                await sio.emit('update_left_click', {'sid': user_sid, 'x': user_info['last_click'][0], 'y': user_info['last_click'][1]}, room=sid)
+            user = user_data[user_sid]
+            await sio.emit('update_position', {'sid': user.sid, 'x': user.x, 'y': user.y}, room=sid)
+            if user.last_click:
+                await sio.emit('update_left_click', {'sid': user.sid, 'x': user.last_click[0], 'y': user.last_click[1]}, room=sid)
     print(f"{sid} has joined room {room}")
+
+@sio.on('get_rooms')
+async def get_rooms(sid):
+    room_data = {}
+    for room, users in rooms.items():
+        room_data[room] = list(users)
+    await sio.emit('room_data', room_data, room=sid)
 
 @sio.on('mouse_position')
 async def mouse_position(sid, data):
     room = data.get('room')
-    if sid not in user_data:
-        user_data[sid] = {}
-    user_data[sid]['x'] = data['x']
-    user_data[sid]['y'] = data['y']
+    user_data[sid].x = data['x']
+    user_data[sid].y = data['y']
     await sio.emit('update_position', {'sid': sid, 'x': data['x'], 'y': data['y']}, room=room)
 
 @sio.on('left_click')
 async def left_click(sid, data):
     room = data.get('room')
-    user_data[sid]['last_click'] = (data['x'], data['y'])
+    user_data[sid].last_click = (data['x'], data['y'])
     await sio.emit('update_left_click', {'sid': sid, 'x': data['x'], 'y': data['y']}, room=room)
 
 @sio.on('disconnect')
 async def disconnect(sid):
-    rooms = sio.rooms(sid)
-    for room in rooms:
+    for room in sio.rooms(sid):
         await sio.leave_room(sid, room)
+        if room in rooms:
+            rooms[room].discard(sid)
     del user_data[sid]
     print(f"{sid} has left the room(s)")
 
